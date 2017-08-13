d02deab0af573f9bcac65d8a5895ef6bb2829ca4
Saurabh <saurabhmishraa95@gmail.com>
2017-08-13 22:14:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-    res.send("article one requested");
+app.get('/article1',function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 app.get('/article2',function(req,res){
     res.send("article two requested");
